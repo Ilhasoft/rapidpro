ba506ffeed4da8075bdbde09ba0c48aa052a2892
Paulo Bernardo <paulo.bernardo@weni.ai>
2025-02-07 11:17:18 -0300

    fix: aws region

--- temba/utils/dynamo/base.py
+++ temba/utils/dynamo/base.py
@@ -21,7 +21,7 @@ def get_client():
             session = boto3.Session(
                 aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
                 aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-                region_name=settings.AWS_REGION,
+                region_name=settings.DYNAMO_AWS_REGION,
             )
         else:  # pragma: no cover
             session = boto3.Session()
